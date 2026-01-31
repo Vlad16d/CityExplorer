e3a53fa147fd22984200a6bfa6bc52379114ecdf
Vlad16d <imaf29323@gmail.com>
2026-01-31 15:41:02 +0100

    Integrate Openweather API for weather data

--- js/app.js
+++ js/app.js
@@ -1,12 +1,34 @@
-const cities = ["Warszawa", "Berlin", "Praga", "Paryż"];
+const API_KEY = "494d69bb9bce4e888693a40067a6eec3";
 
+const cities = ["Warszawa", "Berlin", "Praga", "Paryż"];
 const citiesList = document.getElementById("cities-list");
 
+async function getWeather(city, element) {
+  try {
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=pl`
+    );
+
+    if (!res.ok) {
+      throw new Error("Błąd pobierania danych");
+    }
+
+    const data = await res.json();
+    element.textContent = `${city} – ${data.main.temp}°C`;
+  } catch (error) {
+    element.textContent = `${city} – brak danych`;
+  }
+}
+
 if (citiesList) {
-    cities.forEach(city => {
-        const div = document.createElement("div");
-        div.className = "city";
-        div.textContent = city;
-        citiesList.appendChild(div);
-    });
+  citiesList.innerHTML = "";
+
+  cities.forEach(city => {
+    const div = document.createElement("div");
+    div.className = "city";
+    div.textContent = "Ładowanie...";
+    citiesList.appendChild(div);
+
+    getWeather(city, div);
+  });
 }
